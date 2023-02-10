a90d565876aba3f6e392aa37a53bfaa5e46ba50c
AERIKIM~ <aeri815@naver.com>
2023-02-10 15:15:57 +0900

    commit

---
+++ day0208/ex5.js
@@ -0,0 +1,15 @@
+// 터미널이나 탐색기 열고 node 파일명.확장자 로 실행
+// 보기 - 터미널  cd 0208 첫줄에 작성했음 
+// 자동으로 뜨는 2번째 줄에 node ex5.js (파일명)작성 
+
+console.log("node 로 자바스크립트 실행하기");
+for(var i=1;i<=10;i++){
+    console.log(i);
+}
+
+console.log("--------------------");
+
+for(var i=1;i<=10;i++){
+    if(i%2==0)
+    console.log(i);
+}
